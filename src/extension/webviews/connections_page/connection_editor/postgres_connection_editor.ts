218b7ebef7fd16b90dc301e373a8165f3f5faf09
Will Scullin <willscullin@google.com>
2023-12-13 12:59:59 -0800

    Connections to lit (#363)
    * Connections to lit
    
    * Style cleanup
    
    * Fix parameter label

---
+++ src/extension/webviews/connections_page/connection_editor/postgres_connection_editor.ts
@@ -0,0 +1,240 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import {LitElement, html} from 'lit';
+import {
+  provideVSCodeDesignSystem,
+  vsCodeCheckbox,
+  vsCodeRadio,
+  vsCodeTextField,
+} from '@vscode/webview-ui-toolkit';
+import {PostgresConnectionConfig} from '../../../../common/connection_manager_types';
+import {customElement, property} from 'lit/decorators.js';
+import {styles} from './connection_editor.css';
+
+provideVSCodeDesignSystem().register(
+  vsCodeCheckbox(),
+  vsCodeRadio(),
+  vsCodeTextField()
+);
+
+@customElement('postgres-connection-editor')
+export class PostgresConnectionEditor extends LitElement {
+  static override styles = [styles];
+
+  @property({type: Object})
+  config!: PostgresConnectionConfig;
+  @property()
+  setConfig!: (config: PostgresConnectionConfig) => void;
+
+  @property({attribute: false})
+  showPassword = false;
+
+  override render() {
+    return html`<table>
+      <tbody>
+        <tr>
+          <td class="label-cell">
+            <label>Name:</label>
+          </td>
+          <td>
+            <vscode-text-field
+              value=${this.config.name}
+              @change=${({target: {value}}: {target: HTMLInputElement}) => {
+                this.setConfig({...this.config, name: value});
+              }}
+            ></vscode-text-field>
+          </td>
+        </tr>
+        <tr>
+          <td class="label-cell">
+            <label>Host:</label>
+          </td>
+          <td>
+            <vscode-text-field
+              value=${this.config.host || ''}
+              @change=${({target: {value}}: {target: HTMLInputElement}) => {
+                this.setConfig({...this.config, host: value});
+              }}
+            ></vscode-text-field>
+          </td>
+        </tr>
+        <tr>
+          <td class="label-cell">
+            <label>Port:</label>
+          </td>
+          <td>
+            <vscode-text-field
+              value=${this.config.port ? this.config.port.toString() : ''}
+              @change=${({target: {value}}: {target: HTMLInputElement}) => {
+                this.setConfig({...this.config, port: parseInt(value)});
+              }}
+            ></vscode-text-field>
+          </td>
+        </tr>
+        <tr>
+          <td class="label-cell">
+            <label>Database Name:</label>
+          </td>
+          <td>
+            <vscode-text-field
+              value=${this.config.databaseName || ''}
+              @change=${({target: {value}}: {target: HTMLInputElement}) => {
+                this.setConfig({...this.config, databaseName: value});
+              }}
+            ></vscode-text-field>
+          </td>
+        </tr>
+        <tr>
+          <td class="label-cell">
+            <label>Username:</label>
+          </td>
+          <td>
+            <vscode-text-field
+              value=${this.config.username || ''}
+              @change=${({target: {value}}: {target: HTMLInputElement}) => {
+                this.setConfig({...this.config, username: value});
+              }}
+            ></vscode-text-field>
+          </td>
+        </tr>
+        <tr>
+          <td class="label-cell">
+            <label>Password:</label>
+          </td>
+          <td>
+            ${this.config.useKeychainPassword !== undefined &&
+            html`<div>
+              <vscode-radio
+                value="keychain"
+                .checked=${this.config.useKeychainPassword || false}
+                @change=${({target}: {target: HTMLInputElement}) => {
+                  if (target.checked) {
+                    this.setConfig({
+                      ...this.config,
+                      password: undefined,
+                      useKeychainPassword: true,
+                    });
+                  }
+                }}
+              >
+                Use existing value
+              </vscode-radio>
+            </div>`}
+            <div>
+              <vscode-radio
+                value="none"
+                key="none"
+                .checked=${!this.config.useKeychainPassword &&
+                this.config.password === undefined}
+                @change=${({target}: {target: HTMLInputElement}) => {
+                  if (target.checked) {
+                    this.setConfig({
+                      ...this.config,
+                      password: undefined,
+                      useKeychainPassword:
+                        this.config.useKeychainPassword === undefined
+                          ? undefined
+                          : false,
+                    });
+                  }
+                }}
+              >
+                No password
+              </vscode-radio>
+            </div>
+            <div>
+              <vscode-radio
+                value="specified"
+                key="specified"
+                .checked=${this.config.password !== undefined}
+                @change=${({target}: {target: HTMLInputElement}) => {
+                  if (target.checked) {
+                    this.setConfig({
+                      ...this.config,
+                      password: '',
+                      useKeychainPassword:
+                        this.config.useKeychainPassword === undefined
+                          ? undefined
+                          : false,
+                    });
+                  }
+                }}
+              >
+                Enter a password ${this.config.password !== undefined && ':'}
+              </vscode-radio>
+            </div>
+          </td>
+        </tr>
+        ${this.config.password !== undefined
+          ? html`<tr>
+              <td></td>
+              <td>
+                <vscode-text-field
+                  value=${this.config.password || ''}
+                  @change=${({target: {value}}: {target: HTMLInputElement}) => {
+                    this.setConfig({
+                      ...this.config,
+                      password: value,
+                    });
+                  }}
+                  type=${this.showPassword ? 'text' : 'password'}
+                  placeholder="Optional"
+                ></vscode-text-field>
+              </td>
+              <td style="padding-left: 10px">
+                <vscode-checkbox
+                  .checked=${this.showPassword}
+                  @change=${({target}: {target: HTMLInputElement}) => {
+                    this.showPassword = target.checked;
+                  }}
+                >
+                  Show Password
+                </vscode-checkbox>
+              </td>
+            </tr>`
+          : null}
+        <tr>
+          <td class="label-cell">
+            <label> Connection URL <i>(Advanced)</i>: </label>
+          </td>
+          <td>
+            <vscode-text-field
+              style="width: 40em"
+              value=${this.config.connectionString || ''}
+              @change=${({target: {value}}: {target: HTMLInputElement}) => {
+                this.setConfig({...this.config, connectionString: value});
+              }}
+            ></vscode-text-field>
+          </td>
+        </tr>
+      </tbody>
+    </table>`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'postgres-connection-editor': PostgresConnectionEditor;
+  }
+}
---
+++ src/extension/webviews/connections_page/connection_editor_list.ts
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import {v4 as uuidv4} from 'uuid';
+import {LitElement, css, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import {
+  provideVSCodeDesignSystem,
+  vsCodeButton,
+} from '@vscode/webview-ui-toolkit';
+import {
+  ConnectionBackend,
+  ConnectionConfig,
+  ExternalConnectionConfig,
+  getDefaultIndex,
+} from '../../../common/connection_manager_types';
+import {
+  ConnectionMessageInstallExternalConnection,
+  ConnectionMessageTest,
+} from '../../../common/message_types';
+import './connection_editor/connection_editor';
+import {styles as editorStyles} from './connection_editor/connection_editor.css';
+
+provideVSCodeDesignSystem().register(vsCodeButton());
+
+const styles = css`
+  .button-editor-group {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+  }
+
+  .empty-state-box {
+    margin: 10px;
+    background-color: var(--vscode-list-hoverBackground);
+    padding: 10px;
+    border: 1px solid var(--vscode-contrastBorder);
+    color: var(--foreground);
+    font-family: var(--font-family);
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100px;
+  }
+`;
+
+@customElement('connection-editor-list')
+export class ConnectionEditorList extends LitElement {
+  static override styles = [styles, editorStyles];
+
+  @property({type: Array})
+  connections!: ConnectionConfig[];
+  @property()
+  setConnections!: (connections: ConnectionConfig[], isNew?: boolean) => void;
+  @property()
+  saveConnections!: () => void;
+  @property()
+  testConnection!: (connection: ConnectionConfig) => void;
+  @property({type: Array})
+  testStatuses!: ConnectionMessageTest[];
+  @property()
+  requestServiceAccountKeyPath!: (connectionId: string) => void;
+  @property({type: Array})
+  availableBackends!: ConnectionBackend[];
+  @property()
+  installExternalConnection!: (config: ExternalConnectionConfig) => void;
+  @property({type: Array})
+  installExternalConnectionStatuses!: ConnectionMessageInstallExternalConnection[];
+  @property({attribute: false})
+  selectedId!: string | null;
+
+  @property({attribute: false})
+  dirty = false;
+
+  addConnection() {
+    const id = uuidv4();
+    this.setConnections([
+      ...this.connections,
+      {
+        name: '',
+        backend: this.availableBackends[0],
+        id,
+        isDefault: this.connections.length === 0,
+      },
+    ]);
+    this.selectedId = id;
+  }
+
+  setConfig(config: ConnectionConfig, index: number) {
+    const copy = [...this.connections];
+    config.isGenerated = false;
+    copy[index] = config;
+    this.setConnections(copy);
+    this.dirty = true;
+  }
+
+  makeDefault(defaultIndex: number) {
+    this.setConnections(
+      this.connections.map((connection, index) => {
+        return {...connection, isDefault: index === defaultIndex};
+      })
+    );
+    this.dirty = true;
+  }
+
+  override render() {
+    const defaultConnectionIndex = getDefaultIndex(this.connections);
+
+    return html` <div style="marginTop: 20px">
+      <div class="button-group" style="margin: 10px">
+        <vscode-button @click=${this.addConnection}
+          >New Connection</vscode-button
+        >
+      </div>
+      ${this.connections.map((config, index) =>
+        this.selectedId === config.id
+          ? html`
+              <connection-editor
+                .config=${config}
+                .setConfig=${(newConfig: ConnectionConfig) =>
+                  this.setConfig(newConfig, index)}
+                .deleteConfig=${() => {
+                  this.setConnections(this.connections.splice(index, 1));
+                  this.dirty = true;
+                }}
+                .testConfig=${() => {
+                  this.testConnection(this.connections[index]);
+                }}
+                .testStatus=${[...this.testStatuses]
+                  .reverse()
+                  .find(message => message.connection.id === config.id)}
+                .requestServiceAccountKeyPath=${this
+                  .requestServiceAccountKeyPath}
+                .isDefault=${index === defaultConnectionIndex}
+                .makeDefault=${() => this.makeDefault(index)}
+                .availableBackends=${this.availableBackends}
+                .installExternalConnection=${this.installExternalConnection}
+                .installExternalConnectionStatus=${[
+                  ...this.installExternalConnectionStatuses,
+                ]
+                  .reverse()
+                  .find(message => message.connection.id === config.id)}
+                .setSelectedId=${(selectedId: string | null) => {
+                  this.selectedId = selectedId;
+                }}
+              ></connection-editor>
+            `
+          : html`<div class="connection-editor-box">
+              <b
+                class="connection-title"
+                @click=${() => (this.selectedId = config.id)}
+              >
+                <i className="codicon codicon-chevron-right"></i>
+                CONNECTION: ${config.name || 'Untitled'}
+              </b>
+            </div>`
+      )}
+      ${this.connections.length === 0
+        ? html`<div class="empty-state-box">NO CONNECTIONS</div>`
+        : null}
+      <div class="button-group" style="margin: 10px">
+        <vscode-button
+          @click=${() => {
+            this.dirty = false;
+            this.saveConnections();
+          }}
+          .disabled=${!this.dirty}
+        >
+          Save
+        </vscode-button>
+      </div>
+    </div>`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'connection-editor-list': ConnectionEditorList;
+  }
+}
