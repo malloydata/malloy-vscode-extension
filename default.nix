a0dde57284ea864f7897d47d4bec5b581f313da3
Will Scullin <willscullin@google.com>
2024-01-04 10:16:09 -0800

    Update node to 18 everywhere

--- default.nix
+++ default.nix
@@ -1,1 +1,1 @@
-with import <nixpkgs> {}; stdenv.mkDerivation { name = "malloy"; buildInputs = [ nodejs-16_x google-cloud-sdk git cacert fakeroot]; }
+with import <nixpkgs> {}; stdenv.mkDerivation { name = "malloy"; buildInputs = [ nodejs-18_x google-cloud-sdk git cacert fakeroot]; }
