4e89d27f7ee9c4e5d875603db92ded1aa2b3f2f4
Michael Toy <mtoy@MikeBook.local>
2025-09-19 15:33:19 -0700

    note which packages are in the main npm

--- scripts/malloy-packages.ts
+++ scripts/malloy-packages.ts
@@ -5,9 +5,19 @@ let malloyPackages = Object.keys(packageJson.dependencies).filter(name =>
   name.startsWith('@malloy')
 );
 
-if (process.argv.length === 3) {
+const nonCorePackages = ['@malloydata/malloy-explorer'];
+const args = process.argv.slice(2);
+
+const corePackagesOnly = args.includes('--core-packages-only');
+if (corePackagesOnly) {
+  malloyPackages = malloyPackages.filter(name => !nonCorePackages.includes(name));
+}
+
+const versionArg = args.find(arg => !arg.startsWith('--'));
+if (versionArg) {
   malloyPackages = malloyPackages.map(
-    packageJson => `${packageJson}@${process.argv[2]}`
+    packageName => `${packageName}@${versionArg}`
   );
 }
+
 console.log(malloyPackages.join(' '));
